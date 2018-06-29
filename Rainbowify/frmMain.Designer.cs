766c1ea7cbfbf107ba23f67e1ea24f1b3bae0a62
AlexM-Dev <mac0017@drouinsc.vic.edu.au>
2018-06-30 00:51:01 +1000

    Add project files.

---
+++ Rainbowify/frmMain.Designer.cs
@@ -0,0 +1,65 @@
+﻿namespace Rainbowify {
+    partial class frmMain {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            this.components = new System.ComponentModel.Container();
+            this.btnRainbowify = new System.Windows.Forms.Button();
+            this.tmrRainbow = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // btnRainbowify
+            // 
+            this.btnRainbowify.Location = new System.Drawing.Point(32, 37);
+            this.btnRainbowify.Name = "btnRainbowify";
+            this.btnRainbowify.Size = new System.Drawing.Size(151, 36);
+            this.btnRainbowify.TabIndex = 0;
+            this.btnRainbowify.Text = "Click to RAINBOWIFY";
+            this.btnRainbowify.UseVisualStyleBackColor = true;
+            this.btnRainbowify.Click += new System.EventHandler(this.btnRainbowify_Click);
+            // 
+            // tmrRainbow
+            // 
+            this.tmrRainbow.Tick += new System.EventHandler(this.tmrRainbow_Tick);
+            // 
+            // frmMain
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.ClientSize = new System.Drawing.Size(214, 111);
+            this.Controls.Add(this.btnRainbowify);
+            this.Font = new System.Drawing.Font("Segoe UI Light", 10F);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "frmMain";
+            this.Text = "Rainbowify";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnRainbowify;
+        private System.Windows.Forms.Timer tmrRainbow;
+    }
+}
+
---
+++ Rainbowify/frmMain.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Rainbowify {
+    public partial class frmMain : Form {
+        public frmMain() {
+            InitializeComponent();
+        }
+
+        private void btnRainbowify_Click(object sender, EventArgs e) {
+            this.TopMost = true;
+            btnRainbowify.Visible = false;
+      
+            this.FormBorderStyle = FormBorderStyle.None;
+            this.WindowState = FormWindowState.Maximized;
+
+            tmrRainbow.Start();
+        }
+
+        double i = 0;
+        private void tmrRainbow_Tick(object sender, EventArgs e) {
+            this.BackColor = ColorScale.ColorFromHSL(i, 0.5, 0.5);
+
+            i += 0.01;
+            i = Math.Round(i, 2);
+            if (i == 1) i = 0;
+        }
+    }
+}
